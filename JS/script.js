e90e2ab49274d1ca73be32e717e34036ed55b6bb
rag-san <ragnarson7811@gmail.com>
2025-11-02 21:59:12 -0300

    JavaScript do site - Atividade 3

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    if (hamburger && navLinks) {
+        hamburger.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+            hamburger.classList.toggle('open');
+        });
+    }
+});
